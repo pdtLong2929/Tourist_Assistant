dd3e441d67dc373c4000fa74c4aa8f4627a30787
theelphenomeno10 <theelphenomeno10@gmail.com>
2026-04-09 09:18:50 +0700

    add: /health route for local testing on github actions, fix: github actions login api testing route

--- login/server.js
+++ login/server.js
@@ -15,7 +15,11 @@ connectDB();
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 
-const PORT = process.env.PORT || 5000;
+app.get('/api/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+const PORT = process.env.PORT || 5036;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
